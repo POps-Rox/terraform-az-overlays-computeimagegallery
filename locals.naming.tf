064df8505571aea96c09fb2b4a61d462f82ef903
Josh Luedeman <josh@joshluedeman.com>
2026-04-19 20:18:25 -0400

    fix: correct resource type name to popsrox_resource_name

--- locals.naming.tf
+++ locals.naming.tf
@@ -6,5 +6,5 @@ locals {
 
   resource_group_name        = element(coalescelist(data.azurerm_resource_group.rg.*.name, module.mod_scaffold_rg.*.resource_group_name, [""]), 0)
   location                   = element(coalescelist(data.azurerm_resource_group.rg.*.location, module.mod_scaffold_rg.*.resource_group_location, [""]), 0)
-  compute_image_gallery_name = coalesce(var.custom_compute_image_gallery_name, data.popsrox_utils_resource_name.shared_image_gallery.result)
+  compute_image_gallery_name = coalesce(var.custom_compute_image_gallery_name, data.popsrox_resource_name.shared_image_gallery.result)
 }
